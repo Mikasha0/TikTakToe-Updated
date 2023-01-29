b65d2cb1311a6e9d9e6f311ba04e74ce3efab2ef
Mikasha0 <anikettamrakar0@gmail.com>
2023-01-29 08:43:19 +0545

    made scoreboard in the game

--- tiktactoe/src/App.js
+++ tiktactoe/src/App.js
@@ -1,9 +1,12 @@
 import Board from "./Component/Board";
 import React, { useState } from "react";
+import ScoreBoard from "./Component/ScoreBoard";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  const [gameOver, setGameOver] = useState(false);
   const WIN_CONDITIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,7 +26,19 @@ function App() {
         return value;
       }
     });
-    checkWinner(updateBoard);
+
+    const winner = checkWinner(updateBoard);
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore });
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore });
+      }
+    }
     setBoard(updateBoard);
     setXPlaying(!xPlaying);
   };
@@ -31,16 +46,21 @@ function App() {
   const checkWinner = (board) => {
     for (let i = 0; i < WIN_CONDITIONS.length; i++) {
       const [x, y, z] = WIN_CONDITIONS[i];
-      if (board[x] && board[x] === board[y] && board[z]) {
-        console.log(board[x]);
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        setGameOver(true);
         return board[x];
       }
     }
   };
+  const resetBoard = (board) => {
+    setGameOver(false);
+    setBoard(Array(9).fill(null));
+  };
 
   return (
     <div className="App">
-      <Board board={board} onClick={handleBoxClick} />
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
     </div>
   );
 }
